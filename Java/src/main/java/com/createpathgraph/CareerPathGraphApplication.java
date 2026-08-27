cdcc298743e384478a8b08e7d70f79099769368e
Lenovo <drupad.g2030@gmail.com>
2026-08-28 02:12:47 +0530

    Add Java career path graph API

---
+++ Java/src/main/java/com/createpathgraph/CareerPathGraphApplication.java
@@ -0,0 +1,12 @@
+package com.createpathgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerPathGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerPathGraphApplication.class, args);
+    }
+}
